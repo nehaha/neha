ab3369b313993dd130f3dbab9d8da2e68fccb166
Neha Satapathy <nehaha@nehas-mbp.myfiosgateway.com>
2020-07-31 19:26:40 -0400

    Proper menu icons for toggling sidebar

--- src/main.js
+++ src/main.js
@@ -2,3 +2,15 @@
 function toggleMenu() {
   document.getElementById("menu").classList.toggle('visible');
 }
+
+/* Hide menu icon and toggle sidebar on click */
+function toggleOpenMenu() {
+	document.getElementById('openMenu').style.display = 'none';
+	toggleMenu();
+}
+
+/* Toggle sidebar on click and display menu icon */
+function toggleCloseMenu() {
+	document.getElementById('openMenu').style.display = 'block';
+	toggleMenu();
+}
