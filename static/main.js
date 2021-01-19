405d5b0d5b7f8be5192cbdd2fbee663aa3910474
Neha Satapathy <n.satapathy02@gmail.com>
2021-01-19 01:17:37 -0500

    accidentally overwrote css file T.T

--- static/main.js
+++ static/main.js
@@ -1,16 +1,65 @@
-/* Toggle sidebar*/
-function toggleMenu() {
-  document.getElementById("menu").classList.toggle('visible');
-}
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
-/* Hide menu icon and toggle sidebar on click */
-function toggleOpenMenu() {
-	document.getElementById('openMenu').style.display = 'none';
-	toggleMenu();
-}
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-/* Toggle sidebar on click and display menu icon */
-function toggleCloseMenu() {
-	document.getElementById('openMenu').style.display = 'block';
-	toggleMenu();
-}
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #c8a2c8}";
+    document.body.appendChild(css);
+};
+
+// Select the button
+const btn = document.querySelector(".toggle-item");
+ 
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle("dark-theme");
+});
